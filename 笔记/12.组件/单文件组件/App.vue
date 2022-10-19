0e51b35ef69c436b3ac2981083c872a516531070
dedsec <1115659949@qq.com>
2022-10-19 14:08:19 +0800

    学习记录

---
+++ 笔记/12.组件/单文件组件/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <School></School>
+    <School/>
+  </div>
+</template>
+
+
+<script>
+//引入组件
+import School from "./School";
+
+export default {
+name:'App',
+//注册组件
+  components:{
+  School
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ 笔记/12.组件/单文件组件/School.vue
@@ -0,0 +1,55 @@
+<!--单文件组件的定义-->
+
+
+<!--单文件组件的结构-->
+<template>
+  <div class="msg">message:{{msg}}</div>
+  <div class="name">name:{{name}}</div>
+  <div class="address">address:{{address}}</div>
+  <button>获取信息</button>
+  <button>获取名</button>
+  <button>获取地址</button>
+</template>
+
+<!--组件的交互-->
+<script>
+//暴露接口
+//默认  暴露
+export default {
+  name:'school',
+   data(){
+     return{
+       msg:'hello',
+       name:'十八中',
+       address:'人民路'
+     }
+   },
+   methods:{
+     getMsg(){
+       console.log(this.msg)
+     },
+     getName(){
+       console.log(this.name)
+     },
+     getAddress(){
+       console.log(this.address)
+     }
+   }
+ }
+
+ // export {school}
+
+</script>
+
+<!--组件的样式-->
+<style>
+.msg{
+  background-color:orange;
+}
+.name{
+background-color: #0a58ca;
+}
+.address{
+  background-color: #0dcaf0;
+}
+</style>
