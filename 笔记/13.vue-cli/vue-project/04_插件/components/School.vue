369e945cdcfaa3cb319a46d20b4e689cbed5d60d
dedsec <1115659949@qq.com>
2022-10-29 14:30:32 +0800

    学习记录

---
+++ 笔记/13.vue-cli/vue-project/04_插件/components/School.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>name{{name}}</div>
+    <div>address:{{address}}</div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "School",
+  data(){
+    return {
+      name:'十八种',
+      address:'人民路'
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 笔记/13.vue-cli/vue-project/04_插件/components/Student.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h2>msg:{{msg}}</h2>
+  <div>name:{{name}}</div>
+  <div>age:{{age}}</div>
+  <div>sex:{{sex}}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data(){
+    return {
+      msg:'i am a student',
+      name:'christin',
+      age:18,
+      sex:'male'
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
