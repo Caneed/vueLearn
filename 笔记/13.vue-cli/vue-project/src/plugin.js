a3a2b526c4e81b4bf8a87d8df5e3007ded95287d
dedsec <1115659949@qq.com>
2022-10-29 14:30:36 +0800

    学习记录

--- 笔记/13.vue-cli/vue-project/src/plugin.js
+++ 笔记/13.vue-cli/vue-project/src/plugin.js
@@ -1,8 +1,10 @@
 //调用插件
 
 const obj={
-    install(){
-        console.log('install')
+    //插件中必须使用install方法，install方法接收一个参数，参数为Vue构造函数
+    //在install中可以定义vue实例对象中的任何东西，并且在组件中可以使用
+    install(Vue){
+        console.log('install',Vue)
     }
 }
 
