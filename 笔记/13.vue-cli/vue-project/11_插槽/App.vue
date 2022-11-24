653c59004752099ba8a4693983e15624f21608ae
dedsec <1115659949@qq.com>
2022-11-24 17:28:07 +0800

    学习记录

---
+++ 笔记/13.vue-cli/vue-project/11_插槽/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <category>
+<!--      <img src="/0.jpg" alt="">-->
+
+<!--      使用具名插槽时需要用template标签进行包裹,并指定插槽名:v-slot:插槽名-->
+<!--      或者使用简写形式 #插槽名-->
+      <template v-slot:place1>
+      <img src="/0.jpg" alt="">
+      </template>
+      <template #place2>
+        <div>我是插槽二要插入的标签</div>
+      </template>
+    </category>
+  </div>
+</template>
+
+<script>
+import category from "./components/Category";
+
+export default {
+  name: 'App',
+  components: {
+    category
+  },
+}
+</script>
+
+<style>
+img{
+  width: 100%;
+}
+</style>
--- 笔记/13.vue-cli/vue-project/vue.config.js
+++ 笔记/13.vue-cli/vue-project/vue.config.js
@@ -4,20 +4,20 @@ module.exports = defineConfig({
   // devServer:{
   //   proxy:'http://localhost:5000'
   // }
-  devServer:{
-    proxy:{
-      '/stureq':{
-        target:'http://localhost:5000',
-        pathRewrite:{'^/stureq':''},//表示将前缀的/stureq替换为空，这样请求的接口就是/student而不是/stureq/student了
-        ws: true,//是否启用websocket
-        changeOrigin: true//是否显示发送请求的真实端口
-      },
-      '/carreq':{
-        target:'http://localhost:5001',
-        pathRewrite:{'^/carreq':''},
-        ws: true,//是否启用websocket
-        changeOrigin: true//是否显示发送请求的真实端口
-      }
-    }
-  }
+  // devServer:{
+  //   proxy:{
+  //     '/stureq':{
+  //       target:'http://localhost:5000',
+  //       pathRewrite:{'^/stureq':''},//表示将前缀的/stureq替换为空，这样请求的接口就是/student而不是/stureq/student了
+  //       ws: true,//是否启用websocket
+  //       changeOrigin: true//是否显示发送请求的真实端口
+  //     },
+  //     '/carreq':{
+  //       target:'http://localhost:5001',
+  //       pathRewrite:{'^/carreq':''},
+  //       ws: true,//是否启用websocket
+  //       changeOrigin: true//是否显示发送请求的真实端口
+  //     }
+  //   }
+  // }
 })
